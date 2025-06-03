e38abda61241e6546c5d6c0e738609c7f50b1291
elpekenin <elpekenin@elpekenin.dev>
2025-06-03 15:47:45 +0200

    pre-commit fixes

--- users/elpekenin/include/elpekenin/spi_custom.h
+++ users/elpekenin/include/elpekenin/spi_custom.h
@@ -1,5 +1,7 @@
+// Copyright Pablo Martinez (@elpekenin) <elpekenin@elpekenin.dev>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 /* Copyright 2020 Nick Brassel (tzarc)
- * Copyright Pablo Martinez (@elpekenin) <elpekenin@elpekenin.dev>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
